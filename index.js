cd14d0fc9a98c2e20aceb6e1a667d392a6c99617
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-05-29 19:57:01 +0100

    js

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
 
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                            <td>${order.productName}</td>
+                            <td>${order.productNumber}</td>
+                            <td>${order.payment}</td>
+                            <td class="${order.status === 'Declined' ? 'danger' : order.status === 'pending' ? 'warning' : 'primary'}">${order.status}</td>
+                            <td class="primary">Details</td> `;
+        tr.innerHTML = trContent;
+        document.querySelector('table tbody').appendChild(tr);
+})
---
+++ order.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Chair to go',
+        productNumber: '874321',
+        payment: 'paid',
+        status: 'Delivered'
+    },
+    {
+        productName: 'Iphone 13',
+        productNumber: '344231',
+        payment: 'due',
+        status: 'pending'
+    },
+    {
+        productName: 'Chair to go',
+        productNumber: '874321',
+        payment: 'paid',
+        status: 'Delivered'
+    },
+    {
+        productName: 'Chair to go',
+        productNumber: '874321',
+        payment: 'paid',
+        status: 'Delivered'
+    }
+]
